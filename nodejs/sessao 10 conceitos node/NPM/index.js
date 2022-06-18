9f72816eebc6a4f26de44e6c69dfeb9f8ac38fb6
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2022-06-18 19:12:05 -0300

    Aprendendo NPM

---
+++ nodejs/sessao 10 conceitos node/NPM/index.js
@@ -0,0 +1,12 @@
+// npm init
+// npm install express
+// npm install express -F              -Para manter a versão exata
+// npm install express --save-dev      -Para Salvar como dependência de desenvolvedor
+// npm install express --save-prod     -Para Salvar em dependencias
+// npm install express@2.1.0 -E        -instalar uma versão exata
+// npm uninstall express               -desinstala pacote
+// npm ls                              -Lista os pacotes
+// npm i webpack --save-dev
+// npm outdated                        - Busca pacotes desatualizados
+// npm update                          - Atualiza os pacotes
+// "express": "~2.1.1"         -ele não atualiza acima de 2 exemplo(3.1.1)
