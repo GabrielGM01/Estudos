0d4ad83fec8ddea3af9b39284605a0f2d4943ac1
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2022-06-18 19:56:52 -0300

    Dominando express parte 1

---
+++ nodejs/sessao 10 conceitos node/express aula 1/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//          POST   GET   PUT     DELETE
+
+app.get('/', (req,res)=>{
+    res.send('Hello world');
+});
+
+app.get('/contato', (req,res)=>{
+   res.send('Obrigado por entrar em contato com a gente'); 
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
+
+// node server.js
